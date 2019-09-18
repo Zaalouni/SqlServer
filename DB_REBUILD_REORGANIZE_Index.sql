4548e231a739e4d646916f1af748d2950b40deaa
Zaalouni <43424197+Zaalouni@users.noreply.github.com>
2019-09-18 11:15:30 +0200

    DB_REBUILD_REORGANIZE_Index
    DB_REBUILD_REORGANIZE_Index

---
+++ DB_REBUILD_REORGANIZE_Index.sql
@@ -0,0 +1,25 @@
+--------------------------------------------------------------------------------------
+-- File Name    : https://github.com/Zaalouni/SqlServer
+-- Author       : Zaalouni
+-- Description  : SQL SERVER - REBUILD - REORGANIZE  Index
+-- website      : http://www.aws-senior.com
+-- Github       : https://github.com/Zaalouni/SqlServer
+-- -----------------------------------------------------------------------------------
+
+--- Here is the syntax of the ALTER INDEX Rebuilding index.
+
+	
+ALTER INDEX IndexName ON TableName REBUILD;
+
+--- It is a very simple syntax. Here is another syntax for reorganizing the index.
+
+	
+ALTER INDEX IndexName ON TableName REORGANIZE;
+
+--------------------------------------------------------------------------------------
+-- File Name    : https://github.com/Zaalouni/SqlServer
+-- Author       : Zaalouni
+-- Description  : SQL SERVER - REBUILD - REORGANIZE  Index
+-- website      : http://www.aws-senior.com
+-- Github       : https://github.com/Zaalouni/SqlServer
+-- -----------------------------------------------------------------------------------
